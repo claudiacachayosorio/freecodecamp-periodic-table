a119f56bc6b7053aa376182bc1414c644e441fa1
claudiacachayosorio <claudiacachayosorio.web@gmail.com>
2026-07-06 14:22:16 -0400

    refactor: main bash script formatting and layout

--- element.sh
+++ element.sh
@@ -4,10 +4,8 @@
 # INTERACTIVE PERIODIC TABLE #
 ##############################
 
-
-
-# USER STORIES
-
+# to run the script:
+# ./element.sh <atomic_number|symbol|name>
 
 
 
@@ -20,62 +18,61 @@ IN=$1
 
 
 
+
 # FUNCTIONS
 
 GET_ATOMIC_NUMBER() {
-	# if IN = atomic_number
+	# if input is atomic_number
 	if [[ $IN =~ ^[0-9]+$ ]]
 	then
-		NUMBER_QUERY_CONDITION="atomic_number = $IN"
+		SELECT_NUMBER_CONDITION="atomic_number = $IN"
 
-	# if IN = symbol
+	# if input is symbol
 	elif [[ $IN =~ ^[A-Z][a-z]?$ ]]
 	then
-		NUMBER_QUERY_CONDITION="symbol = '$IN'"
+		SELECT_NUMBER_CONDITION="symbol = '$IN'"
 
-	# if IN = name
+	# if input is name
 	else
-		NUMBER_QUERY_CONDITION="LOWER(name) = LOWER('$IN')"
+		SELECT_NUMBER_CONDITION="LOWER(name) = LOWER('$IN')"
 	fi
 
-	# select atomic_number
-	ATOMIC_NUMBER=$($PSQL "
-		SELECT atomic_number FROM elements WHERE $NUMBER_QUERY_CONDITION;
-	")
+	# get atomic_number
+	ATOMIC_NUMBER=$(
+		$PSQL "SELECT atomic_number FROM elements WHERE $SELECT_NUMBER_CONDITION;"
+	)
 }
 
 GET_ELEMENT_DATA() {
 	# get element_name
-	ELEMENT_NAME=$($PSQL "
-		SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER;
-	")
+	ELEMENT_NAME=$(
+		$PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER;"
+	)
 
 	# get element_symbol
-	ELEMENT_SYMBOL=$($PSQL "
-		SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER;
-	")
+	ELEMENT_SYMBOL=$(
+		$PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER;"
+	)
 
 	# get atomic_mass
-	ATOMIC_MASS=$($PSQL "
-		SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER;
-	")
+	ATOMIC_MASS=$(
+		$PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER;"
+	)
 
 	# get melting_point
-	MELTING_POINT=$($PSQL "
-		SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER;
-	")
+	MELTING_POINT=$(
+		$PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER;"
+	)
 
 	# get boiling_point
-	BOILING_POINT=$($PSQL "
-		SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER;
-	")
+	BOILING_POINT=$(
+		$PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER;"
+	)
 
 	# get element_type
-	ELEMENT_TYPE=$($PSQL "
-		SELECT type FROM types
-		INNER JOIN properties USING (type_id)
-		WHERE atomic_number = $ATOMIC_NUMBER;
-	")
+	ELEMENT_TYPE=$(
+		$PSQL "SELECT type FROM types INNER JOIN properties USING (type_id) WHERE atomic_number = $ATOMIC_NUMBER;"
+	)
 }
 
 ABOUT_ELEMENT() {
@@ -94,7 +91,6 @@ ABOUT_ELEMENT() {
 
 		else
 			GET_ELEMENT_DATA
-
 			echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($ELEMENT_SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
 		fi
 	fi
@@ -102,6 +98,7 @@ ABOUT_ELEMENT() {
 
 
 
-# SCRIPT
+
+# POINT OF ENTRY
 
 ABOUT_ELEMENT
